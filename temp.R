63012353c23a42d547f17bedb61ed648eefeea4c
Anuj Sharma <rknx@outlook.com>
2020-05-05 23:20:32 -0400

    cleansing

--- temp.R
+++ temp.R
@@ -1,34 +1,21 @@
+# Dumnmy scratchbook script to quickly test code
+
 a <- c("gene", "dpi", "dis", "rain", "wind", "temp", "rh", "dew")
 
 vec <- c(F, F, F, T, T, T, T, T, T, T)
 
-terms <- a[vec]
+l <- list(c(1:2), c(3:4))
 
-combn(a, 2)
+list_extract <- function(var, ind) var[[ind]]
 
-comblen <- seq_len(length(terms))
-
-comb <- unlist(lapply(comblen, function(x) combn(comblen, x, simplify = F)),
-    recursive = F
-)
-
-formulae <- sapply(comb, function(x) {
-    paste("y ~", paste(terms[x], collapse = "+"))
-})
-
-for (i in seq_len(length(formulae))) {
-    fit <- glm(formulae[i],
-        data = data,
-        family = binomial(link = "logit")
-    )
+f <- function(list) {
+    lapply(list, function(v, i) v[[i]], 1)
 }
 
-library(ggplot2)
+function(x) {
+    print("[" + x)
+}
 
-weather_2016$row <- seq_len(nrow(weather_2016))
+"[["(l, 1)
 
-ggplot() +
-    geom_point(aes(x = row, y = Temp.2m), data = weather_2016[weather_2016$Month == "May" & weather_2016$Day == "5", ], color = "red") +
-    geom_point(aes(x = row, y = Temp.2m), data = weather_2016[weather_2016$Month == "May" & weather_2016$Day == "6", ], color = "blue") +
-    geom_point(aes(x = row, y = Temp.2m), data = weather_2016[weather_2016$Month == "May" & weather_2016$Day == "7", ], color = "green") +
-    geom_point(aes(x = row, y = Temp.2m), data = weather_2016[weather_2016$Month == "May" & weather_2016$Day == "8", ], color = "black")
+f(l)
