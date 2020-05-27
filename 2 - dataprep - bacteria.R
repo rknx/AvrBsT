3cab2506af292decac0604aedf5caefab566a066
Anuj Sharma <rknx@outlook.com>
2020-05-27 19:51:02 -0400

    implemented dplyr

--- 2 - dataprep - bacteria.R
+++ 2 - dataprep - bacteria.R
@@ -4,8 +4,8 @@
 source("0 - prerequisites.R")
 
 ## Libraries
-"openxlsx" %>=>% lib_install %!=>% library(.., char = T)
-"reshape2" %>=>% lib_install %!=>% library(.., char = T)
+"openxlsx" %>=>% libInstall %!=>% library(.., char = T)
+"reshape2" %>=>% libInstall %!=>% library(.., char = T)
 
 ## Set the working directory
 setwd(rprojroot::find_rstudio_root_file())
@@ -16,11 +16,11 @@ setwd(rprojroot::find_rstudio_root_file())
 
 ## Load excel files
 datafile <- paste0(getwd(), "/Data/data.xlsx")
-"fWT" %=>% read.xlsx(datafile, .., na.strings = "#N/A") %->% field_wt
-"fMut" %=>% read.xlsx(datafile, .., na.strings = "#N/A") %->% field_mut
+"fWT" %=>% read.xlsx(datafile, .., na.strings = "#N/A") %->% fieldWt
+"fMut" %=>% read.xlsx(datafile, .., na.strings = "#N/A") %->% fieldMut
 
 ## Experiment start date
-experiment_start <- c(
+experimentStart <- c(
     `2015` = as.Date("2015-8-26"),
     `2016` = as.Date("2016-3-15"),
     `2017` = as.Date("2017-4-3")
@@ -31,46 +31,45 @@ experiment_start <- c(
 # First appearance -------------------------------------------------------------
 
 ## Determine state change
-
-first_inf <- function(data) {
+firstInfection <- function(data) {
     data %=>% names %!=>% as.numeric %=>% is.na %=>% `!` %=>% data[..] %!=>%
         apply(.., 1, function(x) names(..)[min(which(x > 0))] %=>% as.numeric)
 }
 
 ## State change for each type of input
-field_wt$first_wt <- first_inf(field_wt)
-field_mut$first_mut <- first_inf(field_mut)
+fieldWt$firstWt <- firstInfection(fieldWt)
+fieldMut$firstMut <- firstInfection(fieldMut)
 
 
 
 # Accumulation data for genotypes ----------------------------------------------
 
 ## Function
-accu <- function(data) {
-    data %=>% names %!=>% as.numeric %=>% is.na %=>% `!` %->% col_types
-    data %=>% nrow %=>% seq_len %=>%
-        lapply(.., function(z) {
+transformAccu <- function(.data) {
+    .data %=>% names %!=>% as.numeric %=>% is.na %=>% `!` %->% colTypes
+    .data %=>% nrow %=>% seq_len %=>%
+        lapply(.., z ->> {
             Reduce(
-                function(x, y) ifelse(1 %in% x, 1, y),
-                data[z, col_types],
+                .(x, y) ->> ifelse(1 %in% x, 1, y),
+                .data[z, colTypes],
                 accumulate = TRUE
-            ) %=>% t %=>% data.frame
+            ) %=>% t
         }) %=>%
-        do.call(rbind, ..) %=>%
-        col_names(.., names(data)[col_types]) %=>%
-        data.frame(data[!col_types], .., check.names = F)
+        do.call(rbind.data.frame, ..) %=>%
+        setNames(.., names(.data)[colTypes]) %=>%
+        data.frame(.data[!colTypes], .., check.names = F)
 }
 
 ## Implementation
-field_wt_a <- accu(field_wt)
-field_mut_a <- accu(field_mut)
+fieldWtAccu <- transformAccu(fieldWt)
+fieldMutAccu <- transformAccu(fieldMut)
 
 
 
 # Melt the weeks ---------------------------------------------------------------
 
 ## Function
-melt_weeks <- function(data, val) {
+meltWeeks <- function(data, val) {
     melt(
         data,
         id = names(data)[names(data) %!=>% as.numeric %=>% is.na],
@@ -80,37 +79,31 @@ melt_weeks <- function(data, val) {
 }
 
 ## Implementation
-field_wt_m <- melt_weeks(field_wt_a, "wt")
-field_mut_m <- melt_weeks(field_mut_a, "mut")
+fieldWtMelt <- meltWeeks(fieldWtAccu, "wt")
+fieldMutMelt <- meltWeeks(fieldMutAccu, "mut")
 
 
 
 # Merge all predictors ---------------------------------------------------------
-field_merge <- merge(field_wt_m, field_mut_m, all = T)
+fieldMerge <- merge(fieldWtMelt, fieldMutMelt, all = T)
 
 
 
-# Select right value type ------------------------------------------------------
-field_merge$year <- as.factor(field_merge$year)
-field_merge$rep <- as.factor(field_merge$rep)
-field_merge$week <- as.numeric(as.character(field_merge$week))
-
-
-
-# Some new columns -------------------------------------------------------------
-field_merge$date <-
-    as.Date(experiment_start[field_merge$year]) + field_merge$week * 7
-
-field_merge$first_bac <- pmin(
-    field_merge$first_wt, field_merge$first_mut,
-    na.rm = T
-)
+# Modify data typoe and add some columns ---------------------------------------
+fieldMerge %<=>%
+    mutate(..,
+        year = as.factor(year),
+        rep = as.factor(rep),
+        week = as.numeric(as.character(week)),
+        date = as.Date(experimentStart[year]) + week * 7,
+        firstBac = pmin(firstWt, firstMut, na.rm = T)
+    )
 
 
 
 # Melt genetype ----------------------------------------------------------------
-field_bacteria <- melt(field_merge,
-    id = names(field_merge)[!names(field_merge) %in% c("wt", "mut")],
+fieldBacteria <- melt(fieldMerge,
+    id = names(fieldMerge)[!names(fieldMerge) %in% c("wt", "mut")],
     var = "gene",
     value.name = "presence"
 )
@@ -120,13 +113,13 @@ field_bacteria <- melt(field_merge,
 # Cleaning up ------------------------------------------------------------------
 
 ## Save dataframes as R object
-c("field_bacteria") %=>%
+c("fieldBacteria") %=>%
     save(list = .., file = paste0(getwd(), "/Data/bacteria.rda"))
 
 ## Remove old dataframes
 rm(
-    "field_wt", "field_mut", "field_wt_a", "field_mut_a",
-    "field_wt_m", "field_mut_m", "field_merge"
+    "fieldWt", "fieldMut", "fieldWtAccu", "fieldMutAccu",
+    "fieldWtMelt", "fieldMutMelt", "fieldMerge"
 )
 
 ## Load saved .rda
